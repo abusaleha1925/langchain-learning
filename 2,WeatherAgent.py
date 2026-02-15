176e16e31bbdf0414dada05d56e60961a0ef0d8d
abusaleha1925 <abusaleha1925@gmail.com>
2026-02-16 00:47:36 +0530

    Notes has been added for better accountability

--- 2,WeatherAgent.py
+++ 2,WeatherAgent.py
@@ -1,12 +1,13 @@
 #Single tool usage and example
 
 from langchain_ollama import ChatOllama
-from langchain.agents import create_agent,tool
+from langchain.agents import create_agent,tool               #we need to import create_agent and tool module. create agent is used to create a new agent.
 
 #weather tool function
 
-@tool
+@tool #this is an annotation 
 def get_weather(city: str) -> str:
+
 
     """get weather for a given city"""
     weather_data = {
@@ -16,17 +17,22 @@ def get_weather(city: str) -> str:
         "tumkur": "Sunnny 20"
     }
 
-    return weather_data.get(city.lower(), f"No weather data available for {city}")
+    return weather_data.get(city.lower(), f"No weather data available for {city}") #pass the data in lowercase as again we are here going to convert the data in lowercase 
+
+#In the get_weather function i am adding input cuty becuase in this example i am going to retrive the weather information of the city using our sgent tool only.
 
 
 #connect to local ollama model
-llm = ChatOllama(model="llama3.1")
+llm = ChatOllama (
+    model="llama3.1",
+    temperature = 0
+)                                   #add the model info, keep the temparture = 0 to avoid hallucinations
 
 #Create agent
 agent = create_agent(
-    model=llm,
-    tools=[get_weather],
-    system_prompt="Assistant to fetch weather info"
+    model=llm,                      #adding model info
+    tools=[get_weather],            #tools which needs to called it should be added here
+    system_prompt="Assistant to fetch weather info" #This is the crucial thing!, A system prompt is a high-priority instruction given to the model that defines roles, behaviour, boundaries, how it should respond.
 )
 
 #run agent
--- Agent1.py
+++ Agent1.py
@@ -4,3 +4,5 @@ llm = ChatOllama(model="llama3")
 
 response = llm.invoke("Which team has been qualified for super  in t20 world cup 2026 ")
 print(response.content)
+
+#see this is the simple program here I am using llama3 as llm. if you ask anything if that model has its data, then you will get answer.
